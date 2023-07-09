3958fce45243bc4cbb5e3433dd7cfd8eec5d8795
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2023-07-09 11:36:03 -0300

    esqueleto da função criadora de processos

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+
 using namespace std;
 
 class Process
@@ -9,3 +11,19 @@ public:
     int memoryBlocks;
     string instructions;
 };
+
+// Retorna um processo
+Process createProcess(int memoryBlocks)
+{
+    int id = rand();
+    Process proc;
+    proc.id = id;
+    proc.memoryBlocks = memoryBlocks;
+    return proc;
+}
+
+/*Algumas observações:
+1. Ainda não tenho muita ideia de como fazer a atribuição de instruções a serem executadas por parte do processo
+2. É preciso fazer de uma forma que o comando "create -m X" chame a função createProcess passando X como memoryBlocks
+3. Ainda não tenho muita ideia de onde esse processo vai ser manipulado.
+*/
