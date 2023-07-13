ed8de5c6f316357cf982d8a4fe93dbfad60cab5c
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2023-07-13 14:57:17 -0300

    Remoção de um processo da memoria

--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -2,26 +2,27 @@
 
 using namespace std;
 
-#define MAX_MEMORY 300
+#define MAX_MEMORY_SIZE 300
 
 class Memory
 {
 private:
-    static int memory[MAX_MEMORY];
+    static int memory[MAX_MEMORY_SIZE];
+    static int bitMap[MAX_MEMORY_SIZE];
 
     // Retorna index do primeiro local da memoria livre
-    // ou -1 caso não haja o espaco requisitado disponivel
+    // ou -1 caso nao haja o espaco requisitado disponivel
     int checkEmptyMemorySlot(int requiredMemoryBlocks)
     {
         int startSequenceIndex, lastSequenceIndex;
 
-        for (int i = 0; i < MAX_MEMORY; i++)
+        for (int i = 0; i < MAX_MEMORY_SIZE; i++)
         {
-            if (memory[i] == 0)
+            if (bitMap[i] == 0)
             {
                 startSequenceIndex = i;
 
-                while (memory[i] != 1 && (i - startSequenceIndex) < requiredMemoryBlocks)
+                while (bitMap[i] != 1 && (i - startSequenceIndex) < requiredMemoryBlocks)
                     i++;
 
                 lastSequenceIndex = i;
@@ -36,16 +37,48 @@ private:
         return -1;
     };
 
-public:
-    int insertIntoMemory(int memoryBlocks)
+    // Retorna index do primeiro local da memoria alocada ao processo
+    // ou -1 caso nao encontre o processo na memoria
+    int findProcessInMemory(int processId)
     {
-        int emptyMemorySlotStartIndex = checkEmptyMemorySlot(memoryBlocks);
+        for (int i = 0; i < MAX_MEMORY_SIZE; i++)
+        {
+            if (memory[i] == processId)
+                return i;
+        }
+
+        return -1;
+    }
+
+public:
+    int insertProcessIntoMemory(int requiredMemoryBlocks, int processId)
+    {
+        int emptyMemorySlotStartIndex = checkEmptyMemorySlot(requiredMemoryBlocks);
 
         if (emptyMemorySlotStartIndex < 0)
             return -1;
 
-        for (int i = emptyMemorySlotStartIndex; i < (emptyMemorySlotStartIndex + memoryBlocks); i++)
-            memory[i] = 1;
+        for (int i = emptyMemorySlotStartIndex; i < (emptyMemorySlotStartIndex + requiredMemoryBlocks); i++)
+        {
+            memory[i] = processId;
+            bitMap[i] = 1;
+        }
+
+        return 0;
+    }
+
+    int removeProcessFromMemory(int processId)
+    {
+        int allocatedMemoryForProcessStartIndex = findProcessInMemory(processId);
+
+        if (allocatedMemoryForProcessStartIndex < 0)
+            return -1;
+
+        for (int i = allocatedMemoryForProcessStartIndex; memory[i] == processId; i++)
+        {
+            memory[i] = 0;
+            bitMap[i] = 0;
+        }
 
         return 0;
     }
