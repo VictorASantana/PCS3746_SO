c5d27e7c087311628275a06ac9fe4a540d7023cb
Lucas Garieri <lucaspvg1@gmail.com>
2023-07-26 11:27:47 -0300

    🏗️ 💥 Refactor process source and header

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,5 +1,58 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <fstream>
 #include "process.h"
 
-class Process::Process
+using namespace std;
+
+Process::Process() {}
+
+Process::Process(int memoryBlocks)
 {
+    int id = rand();
+    string nomeArquivo = "./data/instr.txt";
+
+    this->id = id;
+    this->memoryBlocks = memoryBlocks;
+    this->instructions = readInstructions(memoryBlocks, nomeArquivo);
+}
+
+void Process::resume(int pc)
+{
+    this->program_counter = pc;
+}
+
+int Process::block()
+{
+    int pc = this->program_counter;
+    this->program_counter = 0;
+    return pc;
+}
+
+vector<string> Process::readInstructions(int numberOfInstructions, const string &nomeArquivo)
+{
+    vector<string> linhas;
+    ifstream arquivo;
+    string linha;
+
+    arquivo.open(nomeArquivo, ios::in);
+
+    if (arquivo.is_open())
+    {
+        for (int i = 0; i < numberOfInstructions && std::getline(arquivo, linha); i++)
+        {
+            linhas.push_back(linha);
+        }
+
+        arquivo.close();
+    }
+
+    else
+    {
+        std::cout << "Não foi possível abrir o arquivo." << std::endl;
+    }
+
+    linhas.push_back("hlt");
+    return linhas;
 }
--- src/process/process.h
+++ src/process/process.h
@@ -1,64 +1,30 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-#include <iostream>
 #include <string>
-#include <bits/stdc++.h>
-#include <fstream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Process
 {
-public:
+    private:
     int id;
     int memoryBlocks;
     int program_counter;
     vector<string> instructions;
-    void resume(int pc)
-    {
-        this->program_counter = pc;
-    }
-    void block()
-    {
-        this->program_counter = 0;
-    }
+
+    public:
+    Process();
+
+    Process(int);
+
+    void resume(int);
+
+    int block();
+
+    private: vector<string> readInstructions(int, const string&);
 };
-
-std::vector<std::string> readInstructions(int numberOfInstructions, const std::string &nomeArquivo)
-{
-    std::vector<std::string> linhas;
-    std::ifstream arquivo;
-    arquivo.open(nomeArquivo, ios::in);
-    std::string linha;
-
-    if (arquivo.is_open())
-    {
-        for (int i = 0; i < numberOfInstructions && std::getline(arquivo, linha); i++)
-        {
-            linhas.push_back(linha);
-        }
-
-        arquivo.close();
-    }
-    else
-    {
-        std::cout << "Não foi possível abrir o arquivo." << std::endl;
-    }
-    linhas.push_back("hlt");
-    return linhas;
-}
-
-// Retorna um processo
-Process createProcess(int memoryBlocks)
-{
-    int id = rand();
-    Process proc;
-    std::string nomeArquivo = "instr.txt";
-    proc.id = id;
-    proc.memoryBlocks = memoryBlocks;
-    proc.instructions = readInstructions(memoryBlocks, nomeArquivo);
-    return proc;
-}
 
 #endif
