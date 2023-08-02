013e0a08c288cd3df1db3a58ef45685a0becbe88
Lucas Garieri <lucaspvg1@gmail.com>
2023-08-02 01:01:15 -0300

    ✨🚀 Created SO class

---
+++ src/so/so.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "so.h"
+#include "tcb.h"
+#include "process.h"
+
+using namespace std;
+
+OperatingSystem::OperatingSystem() {}
+
+int OperatingSystem::createProcess(int memoryBlocks)
+{
+    Process *p;
+    TCB *t;
+
+    p = new Process(memoryBlocks, this->currentID);
+    t = new TCB(*p);
+
+    this->processVector.push_back(*p);
+    this->TCBVector.push_back(*t);
+    this->mem.insertProcessIntoMemory(*p);
+    this->mem.printMemoryAndBitMap();
+    this->currentID++;
+
+    return 1;
+}
+
+int OperatingSystem::killProcess(int pID)
+{
+    for (int i = 0; i < processVector.size(); i++)
+    {
+        Process p = processVector[i];
+        int id = p.getID();
+
+        if (pID == id)
+        {
+            this->mem.removeProcessFromMemory(p);
+            this->processVector.erase(processVector.begin() + i);
+            this->TCBVector.erase(TCBVector.begin() + i);
+            this->mem.printMemoryAndBitMap();
+            break;
+        }
+    }
+
+    return 1;
+}
+
+Process* OperatingSystem::getProcess(int pid)
+{
+    Process *p;
+    for (int i = 0; i < this->processVector.size(); i++)
+    {
+        p = &processVector[i];
+        if (p->getID() == pid)
+            return p;
+    }
+
+    return NULL;
+}
+
+TCB* OperatingSystem::getTCB(int pID)
+{
+    TCB *t;
+    for (int i = 0; i < TCBVector.size(); i++)
+    {
+        t = &TCBVector[i];
+        int id = t->getProcess().getID();
+        if (pID == id) return t;
+    }
+
+    return NULL;
+}
+
+int OperatingSystem::compactMem()
+{
+    this->mem.fixExternalFragmentation();
+    this->mem.printMemoryAndBitMap();
+    return 1;
+}
+
+int OperatingSystem::runCycle() 
+{
+    Process *p;
+    p = this->getProcess(1);
+    p->updatePC();
+    return (p->getPC());
+}
---
+++ src/so/so.h
@@ -0,0 +1,29 @@
+#include <vector>
+#include "tcb.h"
+#include "process.h"
+#include "memory.h"
+
+using namespace std;
+
+class OperatingSystem {
+    private:
+    Memory mem;
+    int currentID = 1;
+    vector<Process> processVector;
+    vector<TCB> TCBVector;
+
+    public:
+    OperatingSystem();
+
+    int createProcess(int);
+
+    int killProcess(int);
+
+    Process* getProcess(int);
+
+    TCB* getTCB(int);
+
+    int compactMem();
+
+    int runCycle();
+};
